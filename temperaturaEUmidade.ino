54ecc7f6b26274a356dd81caaf8865fcb4d4f039
Rodrigo Wladyka <rodrigo.wladyka@gmail.com>
2022-09-24 11:28:37 -0300

    Tira sd e adiciona display

--- temperaturaEUmidade.ino
+++ temperaturaEUmidade.ino
@@ -1,71 +1,39 @@
 
 #include <SPI.h>
-#include <SdFat.h>
 #include <Ethernet.h>
 #include <DHT.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 #define W5100_CS  10
 #define DHTPIN 7        // Pino digital conectado
 #define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
 
+// DEFINIÇÕES DISPLAY
+#define endereco  0x27 // Endereços comuns: 0x27, 0x3F
+#define colunas   16
+#define linhas    2
+
+// INSTANCIANDO OBJETOS
 DHT dht(DHTPIN, DHTTYPE, 11);
 EthernetClient client;
+LiquidCrystal_I2C lcd(endereco, colunas, linhas);
 
-//----------------- BEGIN SD DEFINITIONS -----------------
-// SD_FAT_TYPE = 0 for SdFat/File as defined in SdFatConfig.h,
-// 1 for FAT16/FAT32, 2 for exFAT, 3 for FAT16/FAT32 and exFAT.
-#define SD_FAT_TYPE 2
-#define USE_ARDUINO_SPI_LIBRARY 1
+// Constantes
+String ID = "0262119";
+String SERVER_ADDR = "homologacao.gestox.com.br"; //Server address
+//String RASP_ADDR; //Raspberry hostname
 
-// SDCARD_SS_PIN is defined for the built-in SD on some boards.
-const uint8_t SD_CS_PIN = 4;
-
-// Try max SPI clock for an SD. Reduce SPI_CLOCK if errors occur.
-#define SPI_CLOCK SD_SCK_MHZ(50)
-
-// Try to select the best SD card configuration.
-#if HAS_SDIO_CLASS
-#define SD_CONFIG SdioConfig(FIFO_SDIO)
-#elif  ENABLE_DEDICATED_SPI
-#define SD_CONFIG SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SPI_CLOCK)
-#else  // HAS_SDIO_CLASS
-#define SD_CONFIG SdSpiConfig(SD_CS_PIN, SHARED_SPI, SPI_CLOCK)
-#endif  // HAS_SDIO_CLASS
-
-#if SD_FAT_TYPE == 0
-SdFat sd;
-File file;
-#elif SD_FAT_TYPE == 1
-SdFat32 sd;
-File32 file;
-#elif SD_FAT_TYPE == 2
-SdExFat sd;
-ExFile file;
-#elif SD_FAT_TYPE == 3
-SdFs sd;
-FsFile file;
-#else  // SD_FAT_TYPE
-#error Invalid SD_FAT_TYPE
-#endif  // SD_FAT_TYPE
-
-char line[40];
-
-//----------------- END SD DEFINITIONS -----------------
-
-String ID;
-String SERVER_ADDR; //Server address
-String RASP_ADDR; //Raspberry hostname
-
-void sendRasp(float h, float t) {
-  if(RASP_ADDR == NULL) {
-    Serial.println(F("Raspberry not configured"));
-    return;
-  }
-  int str_len = RASP_ADDR.length() + 1; 
-  char addr[str_len];
-  RASP_ADDR.toCharArray(addr, str_len);
-  request(h, t, addr);  
-}
+//void sendRasp(float h, float t) {
+//  if(RASP_ADDR == NULL) {
+//    Serial.println(F("Raspberry not configured"));
+//    return;
+//  }
+//  int str_len = RASP_ADDR.length() + 1; 
+//  char addr[str_len];
+//  RASP_ADDR.toCharArray(addr, str_len);
+//  request(h, t, addr);  
+//}
 
 void sendServer(float h, float t) {
   if(SERVER_ADDR == NULL) {
@@ -103,6 +71,8 @@ void request(float h, float t, char host[]) {
 
 
 void readSensor() {
+  lcd.print("T: 15, U: 10");
+  Serial.println("T: 15, U: 10");
   float h = dht.readHumidity(); // Le umidade
   float t = dht.readTemperature(); // Le temperatura
 
@@ -111,100 +81,50 @@ void readSensor() {
     return;
   }
 
-  sendRasp(h, t);
+//  sendRasp(h, t);
   sendServer(h, t);
 }
 
 void setupEthernetShield() {
-
-  Serial.println(F("Start Ethernet"));
+  lcd.clear();
+  lcd.println(F("Start Ethernet"));
   byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };//mac ethernet shield
   if (Ethernet.begin(mac) == 0) {
     while(true);
   }
-  Serial.println(Ethernet.localIP());
+  lcd.print(Ethernet.localIP());
+  delay(2000);
 }
 
-bool parseLine(char* str) {
-
-  // Set strtok start of line.
-  str = strtok(str, "=");
-  if (!str) return false;
-  
-  // Subsequent calls to strtok expects a null pointer.
-  if(strstr(str, "ID")) {
-    str = strtok(nullptr, "=");
-    if (!str) return false;
-    ID = str;
-    ID.trim();
-  } else if(strstr(str, "SERV")) {
-    str = strtok(nullptr, "=");
-    if (!str) return false;
-    SERVER_ADDR = str;
-    SERVER_ADDR.trim();
-  } else if(strstr(str, "RASP")) {
-    str = strtok(nullptr, "=");
-    if (!str) return false;
-    RASP_ADDR = str;
-    RASP_ADDR.trim();
-  } else {
-    str = strtok(nullptr, "=");
-    if (!str) return false;
-  }
-
-  return strtok(NULL, "=") == nullptr;
-}
-
-void readFile() {
-  
-  pinMode(10,OUTPUT); 
-
-  Serial.println(F("Start SD"));
-  // Initialize the SD.
-  if (!sd.begin(SD_CONFIG)) {
-    sd.initErrorHalt(&Serial);
-    sd.end();
-    return;
-  }
-
-  // Create the file.
-  if (!file.open("config.txt", FILE_READ)) {
-    Serial.println(F("open fail"));
-    sd.end();
-    return;
-  }
-
-  while (file.available()) {
-    int n = file.fgets(line, sizeof(line));
-    if (n <= 0) {
-      Serial.println(F("fgets fail"));
-    }
-    if (line[n-1] != '\n' && n == (sizeof(line) - 1)) {
-      Serial.println(F("line so long"));
-    }
-    if (!parseLine(line)) {
-      Serial.println(F("parse failed"));
-    }
-  }
-  
-  file.close();
-  sd.end();
+void setupDisplay() {
+  lcd.init(); // INICIA A COMUNICAÇÃO COM O DISPLAY
+  lcd.begin(16, 2);
+  lcd.backlight(); // LIGA A ILUMINAÇÃO DO DISPLAY
+  lcd.clear(); // LIMPA O DISPLAY
 }
 
 void setup() {
   Serial.begin(9600);
   while (!Serial){;}
-  Serial.println(F("Starting"));
+  setupDisplay();
+  Serial.println(F("Starting..."));
+  lcd.print(F("Starting..."));
   
   setupEthernetShield();
-  readFile();
   dht.begin();
   
+  lcd.clear();
   Serial.println(F("Setup Finished"));
+  lcd.print(F("Setup Finished"));
+  delay(1000);
+  lcd.clear();
+  lcd.print(F("IP: "));
+  lcd.print(Ethernet.localIP());
 }
 
 
 void loop() {
-  delay(60000);
+  delay(6000);
+  lcd.setCursor(0, 1); // POSICIONA O CURSOR NA PRIMEIRA COLUNA DA LINHA 2
   readSensor();
 }
